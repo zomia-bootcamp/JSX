9dfc05617197ce568351160a4d9d059b35aefe80
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-05-15 14:43:54 +0300

    Create paragraph.js

---
+++ exercises/paragraph.js
@@ -0,0 +1,15 @@
+/*
+
+Declare a constant named `p1`.
+
+Set `p1` equal to a JSX `<p>` element containing the text `foo`.
+
+On the next line, declare a constant named `p2`.
+
+Set `p2` equal to another JSX `<p>` element containing the word `bar`.
+
+Give the `<p>` stored in `p1` an `id` attribute of `'large'`.
+
+Give the `<p>` stored in `p2` an `id` attribute of `'small'`.
+
+*/
