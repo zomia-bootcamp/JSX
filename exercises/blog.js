64fbf75789ccc4822e71bf7802eb03b046589697
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-05-15 13:16:13 +0300

    Create blog.js

---
+++ exercises/blog.js
@@ -0,0 +1,17 @@
+/*
+
+Your friend’s blog is down! He’s asked you to fix it. You immediately diagnose the problem: a JSX expression with multiple outer elements.
+
+Repair your friend’s broken code by wrapping the JSX in a <div> element.
+
+*/
+
+const blog = (
+  <img src="pics/192940u73.jpg" />
+  <h1>
+    Welcome to Dan's Blog!
+  </h1>
+  <article>
+    Wow I had the tastiest sandwich today. I <strong>literally</strong> almost freaked out.
+  </article>
+);
